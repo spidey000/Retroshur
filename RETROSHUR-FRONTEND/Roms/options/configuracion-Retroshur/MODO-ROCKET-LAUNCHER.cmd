94831983e94d64c11b24029cc724a3c1d5d84260
Akiafroo <akiafroo@gmail.com>
2017-09-13 14:57:38 +0200

    añadido Selector de Sistema Normal / Rocket-Launcher

---
+++ RETROSHUR-FRONTEND/Roms/options/configuracion-Retroshur/MODO-ROCKET-LAUNCHER.cmd
@@ -0,0 +1,18 @@
+:: script realizado AkiAfroo para Retroshur
+@Echo Off & Cls & Color 0E & Title Retroshur: RETROSHUR-LAUNCHER-ROCKET-LAUNCHER
+set HOME="%~dp0"
+set "MODO-ROCKET-LAUNCHER=%~dp0..\..\..\Emuladores"
+pushd %MODO-ROCKET-LAUNCHER%
+echo.
+echo *******************************************************************************************
+echo el MODO-ROCKET-LAUNCHER significa que rocket-launcher esta activado. Este modo es el recomendado
+echo para equipos Decentes o para quien quiera bezels,overlays o cualquier otro elemento
+echo decorativo.
+echo *******************************************************************************************
+timeout 15
+cls
+echo.
+echo cambiando a Modo Rocket-Launcher:
+echo Hecho!
+timeout 3
+copy RETROSHUR-LAUNCHER-ROCKET-LAUNCHER.shur RETROSHUR-LAUNCHER.cmd
