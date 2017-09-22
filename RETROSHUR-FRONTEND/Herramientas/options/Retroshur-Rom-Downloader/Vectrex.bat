ded17d6129edf816c6979ea3d7de79b1888707f4
Akiafroo <akiafroo@gmail.com>
2017-09-22 11:19:36 +0200

    update gamelist options

---
+++ RETROSHUR-FRONTEND/Herramientas/options/Retroshur-Rom-Downloader/Vectrex.bat
@@ -0,0 +1,32 @@
+:: script realizado por AkiAfroo para Retroshur
+@echo off & Cls & Color 0E & Title Retroshur Rom Downloader: GCE Vetrex (26.3 MB)
+SET HOME="%~dp0"
+SET "CARPETA=%~dp0..\..\..\Roms\"
+SET "EXTRAER=%~dp0..\..\..\Herramientas\7-Zip\7z.exe"
+SET "RETROSHUR=%~dp0..\..\..\Frontends\Emulationstation\"
+SET "BAJAR=%~dp0..\..\..\Herramientas\MEGATOOLS\megadl.exe"
+SET "TEMPORALES=%~dp0..\..\..\.temporales_retroshur_Rom_Downloader\Roms\"
+set "LISTA=https://mega.nz/#!mxUWxYTD!VfEX-gvnjDeRWIIK1O5cdwR3Y5QLPxgyURqRzqeIkeU"
+echo(
+echo Creando carpetas temporales
+if not exist %TEMPORALES% mkdir %TEMPORALES%
+timeout 2 > NUL
+echo Hecho!
+echo Descargando Backup GCE Vectrex para Retroshur
+timeout 2 > NUL
+%BAJAR% --print-names %LISTA% --path %TEMPORALES%
+cd %TEMPORALES%
+%EXTRAER% x vectrex.zip -o%CARPETA% -aoa 
+cd %TEMPORALES%\..\..
+rd /s /q .temporales_retroshur_Rom_Downloader
+CD /D %RETROSHUR%
+
+for /f "tokens=2" %%x in ('tasklist ^| findstr emulationstation.exe') do SET PIDTOKILL=%%x
+start "" "RETROSHUR-FULLSCREEN.cmd" /popup
+taskkill /F /PID %PIDTOKILL% 
+
+)
+
+exit
+
+
