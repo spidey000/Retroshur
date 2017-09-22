441e19dd499de67b6aaa866d70ef2238887c4e40
Akiafroo <akiafroo@gmail.com>
2017-09-22 09:24:48 +0200

    añadiendo Acceso directo para emuladores

--- RETROSHUR-FRONTEND/Herramientas/options/Emuladores/RetroArch.bat
+++ RETROSHUR-FRONTEND/Herramientas/options/Emuladores/RetroArch.bat
@@ -1,6 +1,6 @@
 @echo off
 set HOME="%~dp0"
-cd %~dp0..\..\Emuladores\RetroArch\
+cd %~dp0..\..\..\Emuladores\RetroArch\
 start "" /min /WAIT "retroarch.exe"
 
 exit /b
---
+++ RETROSHUR-FRONTEND/Herramientas/options/Emuladores/Rocket-LauncherUI.bat
@@ -0,0 +1,7 @@
+@echo off
+if not DEFINED IS_MINIMIZED SET IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+set HOME="%~dp0"
+cd %~dp0..\..\..\Emuladores\RocketLauncher\RocketLauncherUI\
+start "" /WAIT "RocketLauncherUI.exe"
+
+exit /b
